115c97dffd520fb3989f349934db4591197bb493
Matt Karl <matt@mattkarl.com>
2018-12-09 19:18:38 -0800

    add webgl test

--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
 const {remote, ipcRenderer} = require('electron')
 const remoteConsole = remote.getGlobal('console')
+const isWebGLSupported = require('./is-webgl-supported');
 
-remoteConsole.log('Renderer started!');
+remoteConsole.log('Renderer started! WebGL:', isWebGLSupported());
 
 ipcRenderer.send('started');
